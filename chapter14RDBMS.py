5c115f6050a4737b597f9676a877577156a1028b
patnir <rahul20patni@gmail.com>
2016-06-10 20:37:09 -0400

    RDBMS notes, pdfs, solutions

---
+++ chapter14RDBMS.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 19:58:29 2016
+
+@author: Rahul Patni
+"""
+
+# Chapter 14 Relational Databases 
+
+# Terminology - Database (or collection of tables), Relation (or table), 
+# Tuple (or row), Attribute (or column)
+
+# SQL - Structured Query Language - issue commands to a database
+# Database application - use of abstraction to communicate with the database
+# APIs exist to help communication
+
+'''
+CREATE TABLE Users (
+name VARCHAR(128), email VARCHAR(128)
+)
+'''
+
