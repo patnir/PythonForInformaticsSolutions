2b1c32592b9503d2c5e12cedede7a87521b0101e
patnir <rahul20patni@gmail.com>
2016-07-03 16:29:17 -0400

    Chapter 15 solutions and will geodata code

---
+++ Chapter15/geodata/where.js
@@ -0,0 +1,17 @@
+myData = [
+[42.340082,-71.0894884, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
+[18.4574518,73.8837999, 'Vishwakarma Institutes Play Ground, Yashodhan Society, Kapil Nagar, Kondhwa Budrukh, Vishwakarma, Maharashtra 411048, India'],
+[42.4036847,-71.120482, 'South Hall Tufts University, 30 Lower Campus Rd, Somerville, MA 02144, USA'],
+[-38.1518106,145.1345412, 'Monash University, Frankston VIC 3199, Australia'],
+[53.2948229,69.4047872, 'Kokshetau 020000, Kazakhstan'],
+[40.7127837,-74.0059413, 'New York, NY, USA'],
+[52.2869741,104.3050183, 'Irkutsk, Irkutsk Oblast, Russia'],
+[31.1790053,121.4237432, 'Shang Hai Jiao Tong Da Xue Fu Shu Di Liu Ren Min Yi Yuan, Xuhui Qu, Shanghai Shi, China, 200231'],
+[8.481302,4.611479, 'University Rd, Ilorin, Nigeria'],
+[-34.9222085,138.5921522, 'Yungondi Building, Adelaide SA 5000, Australia'],
+[47.80949,13.05501, 'Salzburg, Austria'],
+[61.4977524,23.7609535, 'Tampere, Finland'],
+[59.9342802,30.3350986, 'St Petersburg, Russia']
+];
---
+++ Chapter15/notes.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 03 14:57:31 2016
+
+@author: Rahul Patni
+"""
+
+# Chapter 15 - Automating Common Tasks on Your Computer
+
+# Geocoding
+
+# Page rank and web search
+
+# Search Engine Architecture - Web Crawling, Index building and Searching
