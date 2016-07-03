f0b271c6f4d9554b1fcc5a4bb15cd10de5d47d28
patnir <rahul20patni@gmail.com>
2016-07-03 13:02:03 -0400

    Code to parse through the xml file and popluated table

--- Chapter14RDBMS/Week3Assignments/Week3AssignmentSolutions.py
+++ Chapter14RDBMS/Week3Assignments/Week3AssignmentSolutions.py
@@ -44,3 +44,63 @@ CREATE TABLE Track (
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
+
+
+fname = raw_input('Enter a file name: ')
+if (len(fname) < 1): fname = 'Library.xml'
+
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
+    
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+print 'Dict count:', len(all)
+
+for entry in all: 
+    if (lookup(entry, 'Track ID') is None): continue
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    genre = lookup(entry, 'Genre')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+    
+    if name is None or artist is None or album is None or genre is None : 
+        continue
+
+    print name, artist, album, count, rating, length
+    
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+    
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', (genre, ))
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]  
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ? )''', 
+        ( name, album_id, length, rating, count ) )
+
+    conn.commit()
+    
+    
+    
+    
+    
+    
+    
